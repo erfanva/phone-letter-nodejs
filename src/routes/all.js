a083578c8cffcf1e96cd3b9a0b0f49934955c103
erfanva <erfanv1@gmail.com>
2024-08-14 02:18:28 +0330

    routes init

--- src/routes/all.js
+++ src/routes/all.js
@@ -3,7 +3,9 @@ const express = require("express");
 const router = express.Router();
 
 const rootRoute = require("./root");
+const combinationsRoute = require("./combinations");
 
 router.use(rootRoute);
+router.use('/combinations', combinationsRoute);
 
 module.exports = router;
